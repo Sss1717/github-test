f6f2935dd1e65a4d2e2152212b148ff0f9b1ccbc
sss <1390706842@qq.com>
2023-03-09 11:10:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: Hello
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author Sss
+ * @Create 2023/3/9 11:06
+ * @Version 1.0
+ */
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,git01");
+    }
+}
