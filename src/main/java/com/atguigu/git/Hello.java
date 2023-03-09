20c4b2b7510933b107e86cfe0da5553dff83074e
sss <1390706842@qq.com>
2023-03-09 11:14:14 +0800

    second commit

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -14,5 +14,6 @@ package com.atguigu.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello,git01");
+        System.out.println("hello,git02");
     }
 }
